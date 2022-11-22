f95db78ae740068d10082655029a0fcfd0bf2277
Yehor <bob199508@gmail.com>
2022-11-22 23:11:23 +0200

    add scss for different devices screen width

--- src/components/burger-menu/BurgerMenu.jsx
+++ src/components/burger-menu/BurgerMenu.jsx
@@ -1,6 +1,23 @@
-import React, { useState, useContext } from 'react';
-
-export const BurgerMenu = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-}
+// import React, { useState, useContext } from 'react';
+// import { HeaderItems } from '../../config';
+// import styles from './BurgerMenu.module.scss';
+// import menuIcon from 'src/menu-icon/menu-icon.svg';
+// import closeMenuIcon from 'src/menu-icon/close-menu-icon.svg';
+//
+//
+// export const BurgerMenu = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const openedMenu = (
+//       <div className={styles.container}>
+//         <img src={closeMenuIcon} />
+//         <hr/>
+//
+//       </div>
+//   )
+//
+//   return (
+//       <div className={styles.container}>
+//         <img src={menuIcon} />
+//       </div>
+//   )
+// }
